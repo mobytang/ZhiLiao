0f02588ad903f697fa19afccb5613e254b069214
liuqiao1 <350473544@qq.com>
2016-12-10 16:52:49 +0800

    测试回答接口

--- ZhiLiao/src/com/csu/test/testAnswer.java
+++ ZhiLiao/src/com/csu/test/testAnswer.java
@@ -1,6 +1,7 @@
 package com.csu.test;
 
-import org.eclipse.jdt.internal.compiler.ast.ThisReference;
+
+import java.util.List;
 
 import com.csu.bean.Answer;
 import com.csu.dao.AnswerDao;
@@ -17,7 +18,12 @@ public class testAnswer {
 	public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	    testAnswer test=new testAnswer();
-	    test.testDelete();
+	   // test.testInsert();
+	   // test.testDelete();
+	   // test.testUpdate();
+	   // test.testFindAnswerById();
+	   // test.testFindAnswersByQuestion();
+	    test.testFindAnswersByUser();
 	}
 	
 	public void testInsert() {
@@ -38,6 +44,36 @@ public class testAnswer {
 		answer.setAnswerId(5);
 		boolean result=answerDao.deleteAnswer(answer);
 		System.out.println(result);
+	}
+	
+	public void testUpdate() {
+	   Answer answer=new Answer();
+	   answer.setAnswerContent("这是新的");
+	   answer.setAnswerDate(TimeUtil.getCurrentTime());
+	   //answer.setAnswerSupport(0);
+	   answer.setIsAnoy(1);
+	   
+	   AnswerDao answerDao=new AnswerDaoImpl();
+	   boolean result=answerDao.updateAnswer(3, answer);
+	   System.out.println(result);
+	}
+	
+	public void testFindAnswerById() {
+		AnswerDao answerDao=new AnswerDaoImpl();
+		Answer answer=answerDao.getAnswerById(3);
+		System.out.println(answer);
+	}
+	
+	public void testFindAnswersByQuestion() {
+		AnswerDao answerDao=new AnswerDaoImpl();
+		List<Answer> answers=answerDao.getAnswerByQuestionId(1);
+		System.out.println(answers);
+	}
+	
+	public static void testFindAnswersByUser() {
+		AnswerDao answerDao=new AnswerDaoImpl();
+		List<Answer> answers=answerDao.getAnswerByUserId(1);
+		System.out.println(answers);
 	}
 
 }
